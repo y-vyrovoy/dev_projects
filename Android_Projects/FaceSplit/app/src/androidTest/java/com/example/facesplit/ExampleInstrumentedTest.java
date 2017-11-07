19821d5768c03048e64ddb70883a2d14dd2cfe6a
y-vyrovoy <y.vyrovoy@gmail.com>
2017-11-07 20:15:18 +0200

    bug fixed

--- Android_Projects/FaceSplit/app/src/androidTest/java/com/example/facesplit/ExampleInstrumentedTest.java
+++ Android_Projects/FaceSplit/app/src/androidTest/java/com/example/facesplit/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.itamarmedical.ble.facesplit", appContext.getPackageName());
+        assertEquals("com.example.facesplit", appContext.getPackageName());
     }
 }
