fd3a27b2ea4c55f335b7cc1d32dbca74ca563eb5
y-vyrovoy <y.vyrovoy@gmail.com>
2018-09-20 09:50:40 +0300

    Starting to test ResponseDispatcher

--- CPP_projects/RapidBackend/sources/ResponseDispatcher.cpp
+++ CPP_projects/RapidBackend/sources/ResponseDispatcher.cpp
@@ -9,146 +9,146 @@ RequestIdType ResponseDispatcher::m_nextRequestID;
 
 void ResponseDispatcher::registerRequest( SOCKET sock )
 {
-	RequestIdType id = m_nextRequestID++;
+    RequestIdType id = m_nextRequestID++;
 
-	std::unique_lock<std::mutex> lck( m_requestMutex );
+    std::unique_lock<std::mutex> lck( m_requestMutex );
 
-	// mapping request with socket
-	m_requestId2SocketMap[id] = sock;
+    // mapping request with socket
+    m_requestId2SocketMap[id] = sock;
 
-	// updating response chain
-	m_requestsChains[sock].push_back( id );
+    // updating response chain
+    m_requestsChains[sock].push_back( id );
 }
 
 void ResponseDispatcher::registerResponse( ResponsePtr response )
 {
-	RequestIdType id = response->id;
+    RequestIdType id = response->id;
 
-	std::unique_lock<std::mutex> lck( m_responseMutex );
+    std::unique_lock<std::mutex> lck( m_responseMutex );
 
-	if ( m_responses.find( id ) != m_responses.end() )
-	{
-		THROW_MESSAGE << "Duplicating response id #" << id;
-	}
+    if ( m_responses.find( id ) != m_responses.end() )
+    {
+        THROW_MESSAGE << "Duplicating response id #" << id;
+    }
 
-	auto itID = m_requestId2SocketMap.find( id );
-	if ( itID == m_requestId2SocketMap.end() )
-	{
-		THROW_MESSAGE << "Can't find socket for the response id #" << id;
-	}
-	
-	SOCKET sock = itID->second;
+    auto itID = m_requestId2SocketMap.find( id );
+    if ( itID == m_requestId2SocketMap.end() )
+    {
+        THROW_MESSAGE << "Can't find socket for the response id #" << id;
+    }
 
-	m_responses[id] = std::move(response);
+    SOCKET sock = itID->second;
 
-	// if request is the first in chain it should be placed to general response queue 
-	putTopOfChainToQueue(sock);
+    m_responses[id] = std::move(response);
+
+    // if request is the first in chain it should be placed to general response queue 
+    putTopOfChainToQueue(sock);
 }
 
 SOCKET ResponseDispatcher::getSocket( RequestIdType id ) const
 {
-	auto it = m_requestId2SocketMap.find(id);
-	if ( it == m_requestId2SocketMap.end() )
-	{
-		THROW_MESSAGE << "Can't find socket for the response id #" << id;
-	}
+    auto it = m_requestId2SocketMap.find(id);
+    if ( it == m_requestId2SocketMap.end() )
+    {
+            THROW_MESSAGE << "Can't find socket for the response id #" << id;
+    }
 
-	return it->second;
+    return it->second;
 }
 
 void ResponseDispatcher::putTopOfChainToQueue( SOCKET sock )
 {
-	//std::unique_lock<std::mutex> lck( m_responseMutex );
+    //std::unique_lock<std::mutex> lck( m_responseMutex );
 
-	auto itSock = m_requestsChains.find( sock );
-	if (itSock == m_requestsChains.end() )
-	{
-		THROW_MESSAGE << "Can't find socket #" << sock;
-	}
+    auto itSock = m_requestsChains.find( sock );
+    if (itSock == m_requestsChains.end() )
+    {
+        THROW_MESSAGE << "Can't find socket #" << sock;
+    }
 
-	RequestIdType topId = itSock->second.front();
+    RequestIdType topId = itSock->second.front();
 
-	if ( !m_responseQueue.contains( topId ) )
-	{
-		m_responseQueue.push(topId);
-	}
+    if ( !m_responseQueue.contains( topId ) )
+    {
+        m_responseQueue.push(topId);
+    }
 }
 
 ResponseData * ResponseDispatcher::getTopResponse()
 {
-	RequestIdType id = m_responseQueue.pull();
-	return m_responses[id].get();
+    RequestIdType id = m_responseQueue.pull();
+    return m_responses[id].get();
 }
 
 void ResponseDispatcher::removeResponse( RequestIdType id )
 {
-	SOCKET sock = INVALID_SOCKET;
+    SOCKET sock = INVALID_SOCKET;
 
-	{
-		std::unique_lock<std::mutex> lck( m_requestMutex );
+    {
+        std::unique_lock<std::mutex> lck( m_requestMutex );
 
-		auto itID = m_requestId2SocketMap.find( id );
-		if ( itID == m_requestId2SocketMap.end() )
-		{
-			THROW_MESSAGE << "Can't find socket for the response id #" << id;
-		}
+        auto itID = m_requestId2SocketMap.find( id );
+        if ( itID == m_requestId2SocketMap.end() )
+        {
+            THROW_MESSAGE << "Can't find socket for the response id #" << id;
+        }
 
-		sock = itID->second;
+        sock = itID->second;
 
-		auto itSock = m_requestsChains.find( sock );
-		if ( itSock == m_requestsChains.end() )
-		{
-			THROW_MESSAGE << "Can't find socket #" << sock;
-		}
+        auto itSock = m_requestsChains.find( sock );
+        if ( itSock == m_requestsChains.end() )
+        {
+            THROW_MESSAGE << "Can't find socket #" << sock;
+        }
 
-		itSock->second.remove( id );
-	}
+        itSock->second.remove( id );
+    }
 
-	{
-		std::unique_lock<std::mutex> lck( m_responseMutex );
-	
-		m_responses.erase( m_responses.find( id ) );
+    {
+        std::unique_lock<std::mutex> lck( m_responseMutex );
 
-		m_requestId2SocketMap.erase( m_requestId2SocketMap.find( id ) );
+        m_responses.erase( m_responses.find( id ) );
 
-		if ( sock != INVALID_SOCKET )
-		{
-			putTopOfChainToQueue( sock );
-		}
-	}
+        m_requestId2SocketMap.erase( m_requestId2SocketMap.find( id ) );
+
+        if ( sock != INVALID_SOCKET )
+        {
+                putTopOfChainToQueue( sock );
+        }
+    }
 }
 
 
 #include <iostream>
 void ResponseDispatcher::Dump()
 {
-	auto printList = [&](const std::pair<SOCKET, std::list<RequestIdType>> & item)
-	{
-		std::cout << item.first << ": ";
-		std::for_each(item.second.begin(), item.second.end(), [&](const RequestIdType & i) {std::cout << i << " "; });
-		std::cout << std::endl;
-	};
+    auto printList = [&](const std::pair<SOCKET, std::list<RequestIdType>> & item)
+    {
+            std::cout << item.first << ": ";
+            std::for_each(item.second.begin(), item.second.end(), [&](const RequestIdType & i) {std::cout << i << " "; });
+            std::cout << std::endl;
+    };
 
-	std::for_each(m_requestsChains.begin(), m_requestsChains.end(), printList);
-	std::cout << std::endl;
-
-
-
-	std::cout << "id vs sockets: ";
-	std::for_each(m_requestId2SocketMap.begin(),
-					m_requestId2SocketMap.end(),
-					[&](const auto & t) {std::cout << "[" << t.first << ":" << t.second << "] "; });
-	std::cout << std::endl;
+    std::for_each(m_requestsChains.begin(), m_requestsChains.end(), printList);
+    std::cout << std::endl;
 
 
-	std::cout << "Stored responses: ";
-	std::for_each(m_responses.begin(),
-					m_responses.end(),
-					[&](const auto & t) {std::cout << t.first << " "; });
-	std::cout << std::endl;
+
+    std::cout << "id vs sockets: ";
+    std::for_each(m_requestId2SocketMap.begin(),
+                    m_requestId2SocketMap.end(),
+                    [&](const auto & t) {std::cout << "[" << t.first << ":" << t.second << "] "; });
+    std::cout << std::endl;
 
 
-	std::cout << "Top responses: " << m_responseQueue.Dump() << std::endl << std::endl;
-	std::cout << " ======================== " << std::endl << std::endl;
+    std::cout << "Stored responses: ";
+    std::for_each(m_responses.begin(),
+                    m_responses.end(),
+                    [&](const auto & t) {std::cout << t.first << " "; });
+    std::cout << std::endl;
+
+
+    std::cout << "Top responses: " << m_responseQueue.Dump() << std::endl << std::endl;
+    std::cout << " ======================== " << std::endl << std::endl;
 
 }
--- CPP_projects/RapidBackend/sources/main.cpp
+++ CPP_projects/RapidBackend/sources/main.cpp
@@ -1,80 +1,159 @@
 #include "stdafx.h"
 
-#include "ServerFramework.h"
+#include <chrono>
+#include <thread>
 
+#include "ServerFramework.h"
 #include "MessageException.h"
+#include "ResponseDispatcher.h"
+
+#define N_SOCKETS 4
+
+ResponseDispatcher g_dispatcher;
+
+std::atomic<bool> g_stop;
+std::atomic<unsigned int> g_value;
+
+using namespace std::chrono_literals;
+
+void pushRequestThreadFunc()
+{
+    auto threadID = std::this_thread::get_id();
+    for (int i = 0; i < 20; ++i)
+    {
+        DEBUG_LOG << "push" 
+                    " [ thread: " << threadID << " ]"
+                    " [ request " << g_value << " ]" 
+                << std::endl;
+        
+        g_dispatcher.registerRequest( ( g_value++ ) % N_SOCKETS );
+        
+        std::this_thread::sleep_for(50ms);
+    }
+}
+
+void pushResponseThreadFunc()
+{
+
+}
+
+
+void pullThreadFunc()
+{
+    
+    while( !g_stop )
+    {
+        ResponseData * p = g_dispatcher.getTopResponse();
+        auto id = p->id;
+
+        DEBUG_LOG << "Pulling response # " << id;
+
+        if ( id % 7 != 0 )
+        {
+            g_dispatcher.removeResponse( id );
+        }
+        else
+        {
+            g_dispatcher.putTopOfChainToQueue( g_dispatcher.getSocket( id ) );
+        }
+        
+        std::this_thread::sleep_for(20ms);
+    }
+}
+
 
 int main(int argc, char** argv)
 {
-	ResponseDispatcher disp;
-
-	disp.registerRequest(0);
-	disp.registerRequest(1);
-	disp.registerRequest(2);
-	disp.registerRequest(0);
-	disp.registerRequest(1);
-	disp.registerRequest(2);
-	disp.registerRequest(0);
-	disp.registerRequest(1);
-
-	disp.Dump();
-
-	std::cout << "LET'S RUN !" << std::endl 
-				<< std::endl 
-				<< " ------ +++++++++++++++++ -----------" 
-				<< std::endl 
-				<< std::endl;
-
-	try
-	{
-		ResponsePtr response0 = std::make_unique<ResponseData>(ResponseData());
-		response0->id = 0;
-		disp.registerResponse( std::move(response0) );
-
-		std::cout << "Register response id #0" << std::endl;
-		std::cout << "-----------------------" << std::endl << std::endl;	
-		disp.Dump();
-
-
-		ResponsePtr response3 = std::make_unique<ResponseData>(ResponseData());
-		response3->id = 3;
-		disp.registerResponse(std::move(response3));
-
-		std::cout << "Register response id #3" << std::endl;
-		std::cout << "-----------------------" << std::endl << std::endl;
-		disp.Dump();
-
-		ResponsePtr response6 = std::make_unique<ResponseData>(ResponseData());
-		response6->id = 6;
-		disp.registerResponse(std::move(response6));
-
-		std::cout << "Register response id #6" << std::endl;
-		std::cout << "-----------------------" << std::endl << std::endl;
-		disp.Dump();
-
-
-		ResponseData * p = disp.getTopResponse();
-		std::cout << "Pull response" << std::endl;
-		std::cout << "-----------------------" << std::endl << std::endl;
-		disp.Dump();
-
-		disp.putTopOfChainToQueue( disp.getSocket(0) );
-		std::cout << "putTopResponseToQueue(0)" << std::endl;
-		std::cout << "-----------------------" << std::endl << std::endl;
-		disp.Dump();
-
-		disp.getTopResponse();
-		disp.removeResponse(0);
-		std::cout << "pullResponse() -> removeResponse(0)" << std::endl;
-		std::cout << "-----------------------" << std::endl << std::endl;
-		disp.Dump();
-	}
-	catch (std::exception & ex)
-	{
-		std::cout << ex.what() << std::endl;
-	}
-
+    g_stop = false;
+    
+    std::thread thIn1( pushRequestThreadFunc );
+    //std::thread thIn2( pushThreadFunc );
+    
+    std::thread thOut( pullThreadFunc );
+       
+    for (std::string s; std::cin >> s; )
+    {
+            if (s == "exit")
+            break;
+    }
+    
+    thIn1.join();
+    //thIn2.join();
+    
+    thOut.join();
 }
+
+//int main(int argc, char** argv)
+//{
+//	ResponseDispatcher disp;
+//
+//	disp.registerRequest(0);
+//	disp.registerRequest(1);
+//	disp.registerRequest(2);
+//	disp.registerRequest(0);
+//	disp.registerRequest(1);
+//	disp.registerRequest(2);
+//	disp.registerRequest(0);
+//	disp.registerRequest(1);
+//
+//	disp.Dump();
+//
+//	std::cout << "LET'S RUN !" << std::endl 
+//				<< std::endl 
+//				<< " ------ +++++++++++++++++ -----------" 
+//				<< std::endl 
+//				<< std::endl;
+//
+//	try
+//	{
+//		ResponsePtr response0 = std::make_unique<ResponseData>(ResponseData());
+//		response0->id = 0;
+//		disp.registerResponse( std::move(response0) );
+//
+//		std::cout << "Register response id #0" << std::endl;
+//		std::cout << "-----------------------" << std::endl << std::endl;	
+//		disp.Dump();
+//
+//
+//		ResponsePtr response3 = std::make_unique<ResponseData>(ResponseData());
+//		response3->id = 3;
+//		disp.registerResponse(std::move(response3));
+//
+//		std::cout << "Register response id #3" << std::endl;
+//		std::cout << "-----------------------" << std::endl << std::endl;
+//		disp.Dump();
+//
+//		ResponsePtr response6 = std::make_unique<ResponseData>(ResponseData());
+//		response6->id = 6;
+//		disp.registerResponse(std::move(response6));
+//
+//		std::cout << "Register response id #6" << std::endl;
+//		std::cout << "-----------------------" << std::endl << std::endl;
+//		disp.Dump();
+//
+//
+//		ResponseData * p = disp.getTopResponse();
+//		std::cout << "Pull response # " << p->id << std::endl;
+//		std::cout << "-----------------------" << std::endl << std::endl;
+//		disp.Dump();
+//
+//		disp.putTopOfChainToQueue( disp.getSocket(0) );
+//		std::cout << "putTopOfChainToQueue(0)" << std::endl;
+//		std::cout << "-----------------------" << std::endl << std::endl;
+//		disp.Dump();
+//
+//		disp.getTopResponse();
+//		disp.removeResponse(0);
+//		std::cout << "pullResponse() -> removeResponse(0)" << std::endl;
+//		std::cout << "-----------------------" << std::endl << std::endl;
+//		disp.Dump();
+//	}
+//	catch (std::exception & ex)
+//	{
+//		std::cout << "Exception: " << ex.what() << std::endl;
+//	}
+//
+//}
 
 //int main(int argc, char** argv)
 //{
